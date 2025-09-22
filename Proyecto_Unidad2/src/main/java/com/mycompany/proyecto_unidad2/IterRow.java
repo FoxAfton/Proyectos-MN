0078c9fc17bac05ede7f021440968366390110ac
FoxAfton <dalegriavelazquez@gmail.com>
2025-09-21 22:24:29 -0700

    Add files via upload

---
+++ Proyecto_Unidad2/src/main/java/com/mycompany/proyecto_unidad2/IterRow.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_unidad2;
+
+/**
+ *
+ * @author Fox
+ */
+class IterRow {
+        int iter;
+        Double xi, xf, xr, fx, ea;
+
+        IterRow(int iter, Double xi, Double xf, Double xr, Double fx, Double ea) {
+            this.iter = iter;
+            this.xi = xi;
+            this.xf = xf;
+            this.xr = xr;
+            this.fx = fx;
+            this.ea = ea;
+        }
+    }
+
